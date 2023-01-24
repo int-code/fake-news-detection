8ccf70b960b21f9067337a4a9c4df37fcfba3055
int-code <pubalibasak.cse2020@nsec.ac.in>
2023-01-24 19:23:39 +0530

    added model and files

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-import nltk
 import pandas as pd
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-import re
+from setup import stem, model, titlevectorizer, vectorizer
+
+def check(news):
+    title = stem(news['title'])
+    text = stem(news['text'])
+    print(title, text)
+    title = titlevectorizer.transform([news['title']])
+    title = pd.DataFrame.sparse.from_spmatrix(title)
+    # print(title)
+
+    # print("hsdh")
+    text = vectorizer.transform([news['text']])
+    text = pd.DataFrame.sparse.from_spmatrix(text)
+    text.columns = [x for x in range(50,100)]
+    title = pd.merge(title, text, left_index=True, right_index=True)
+    print(title)
+    res = model.predict(title)
+    print(res)
 
 
-print("Starting pre-processing...")
-train_data = pd.read_csv('train.csv')
-train_data.fillna('', inplace=True)
-
-port_stem = PorterStemmer()
-
-def stem(content):
-    stemmed = re.sub('[^a-zA-Z ]', '', content)
-    stemmed = stemmed.lower()
-    stemmed = stemmed.split(" ")
-    stemmed = [port_stem.stem(x) for x in stemmed if not x in stopwords.words('english')]
-    stemmed = " ".join(stemmed)
-    return stemmed
-
-train_data['title'] = train_data['title'].apply(stem)
-print("Title stemmed")
-train_data['text'] = train_data['text'].apply(stem)
-print("Text stemmed")
-
-train_data.to_csv('modified_train.csv')
+check({
+    'title':"Can Republicans Expunge Donald Trump’s Impeachments?",
+    'text':'Republicans unhappy with the impeachments of former President Donald Trump have for years been vocal about their outrage. But during the last Congress, a group of more than two dozen lawmakers in the House went even further, introducing resolutions to “expunge” the impeachments. And now that Republicans have wrested back the majority in the chamber, House Speaker Kevin McCarthy says he’s willing to consider such an effort. “I understand why individuals want to do it, and we’d look at it,” McCarthy, California Republican, said in response to a question earlier this month during his first weekly news conference. But his comments raised questions of their own. Namely: Is expunging Trump’s impeachments even possible?The answer, constitutional experts say, is complicated.For one thing, it would be unprecedented in this context.According to the House historian, impeachment proceedings have been initiated more than 60 times in U.S. history against office holders across the federal government, and less than a third of the proceedings have led to full impeachments. That figure includes 15 federal judges, an 18th century senator and a 19th century Cabinet official along with, most notably, President Andrew Johnson in 1868, President Bill Clinton in 1998 and Trump. Eight impeachments – all of them of federal judges – have led to convictions and removals from office by the Senate. None has ever been expunged.'
+})
--- setup.py
+++ setup.py
@@ -3,27 +3,58 @@ import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
-print("Downloading stopwords...")
-nltk.download('stopwords')
-print("Stopwords downloaded")
-
-print("Starting pre-processing...")
-train_data = pd.read_csv('train.csv')
-train_data.fillna('', inplace=True)
 
 port_stem = PorterStemmer()
+model = LogisticRegression()
+titlevectorizer = TfidfVectorizer(max_features=50)
+vectorizer = TfidfVectorizer(max_features=50)
+
 
 def stem(content):
-    stemmed = re.sub('[^a-zA-Z]', '', content)
+    stemmed = re.sub('[^a-zA-Z ]', '', content)
     stemmed = stemmed.lower()
-    stemmed = stemmed.split()
+    stemmed = stemmed.split(" ")
+    if len(stemmed)>50:
+        stemmed=stemmed[:50]
     stemmed = [port_stem.stem(x) for x in stemmed if not x in stopwords.words('english')]
-    stemmed = ' '.join(stemmed)
+    stemmed = " ".join(stemmed)
     return stemmed
 
-train_data['title'] = train_data['title'].apply(stem)
-print("Title stemmed")
-train_data['text'] = train_data['text'].apply(stem)
-print("Text stemmed")
+if __name__=="__main__":
+    print("Downloading stopwords...")
+    nltk.download('stopwords')
+    print("Stopwords downloaded")
+    print("Starting pre-processing...")
+    train_data = pd.read_csv('train.csv')
+    train_data.fillna('', inplace=True)
 
+    train_data['title'] = train_data['title'].apply(stem)
+    print("Title stemmed")
+    train_data['text'] = train_data['text'].apply(stem)
+    print("Text stemmed")
+
+    # train_data.to_csv('modified_train.csv')
+    # train = pd.read_csv('modified_train.csv')
+    # train.fillna('', inplace=True)
+
+    titlevectorizer.fit(train_data['title'].values)
+    title = titlevectorizer.transform(train_data['title'].values)
+    title = pd.DataFrame.sparse.from_spmatrix(title)
+    # print(title)
+
+    # print("hsdh")
+    vectorizer.fit(train_data['text'].values)
+    text = vectorizer.transform(train_data['text'].values)
+    text = pd.DataFrame.sparse.from_spmatrix(text)
+    text.columns = [x for x in range(50,100)]
+    title = pd.merge(title, text, left_index=True, right_index=True)
+    # print(title)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(title, train_data['label'], test_size = 0.2, stratify=train_data['label'], random_state=2)
+
+    model.fit(X_train, Y_train)
