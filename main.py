aa750a3952d9050879b5e1891c4c4e670ad45098
int-code <pubalibasak.cse2020@nsec.ac.in>
2023-01-24 20:04:45 +0530

    Updated with proper formatting

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import pandas as pd
-from setup import stem, model, titlevectorizer, vectorizer
+import pickle
+from setup import stem
 
-def check(news):
+# Input the title and text of the news article
+TITLE = ''
+TEXT = ''
+
+
+# Loading from storage
+titlevectorizer = pickle.load(open("titlevectorizer.pickle", "rb"))
+vectorizer = pickle.load(open("vectorizer.pickle", "rb"))
+model = pickle.load(open("model.pickle", "rb"))
+
+
+# For passing input data through model
+def check(news:dict):
+
+    # Stemming text
     title = stem(news['title'])
     text = stem(news['text'])
-    print(title, text)
+
+    # Vectorizing
     title = titlevectorizer.transform([news['title']])
     title = pd.DataFrame.sparse.from_spmatrix(title)
-    # print(title)
 
-    # print("hsdh")
     text = vectorizer.transform([news['text']])
     text = pd.DataFrame.sparse.from_spmatrix(text)
+
+    # Merging
     text.columns = [x for x in range(50,100)]
     title = pd.merge(title, text, left_index=True, right_index=True)
-    print(title)
+
     res = model.predict(title)
-    print(res)
+    return res
 
 
-check({
-    'title':"Can Republicans Expunge Donald Trump’s Impeachments?",
-    'text':'Republicans unhappy with the impeachments of former President Donald Trump have for years been vocal about their outrage. But during the last Congress, a group of more than two dozen lawmakers in the House went even further, introducing resolutions to “expunge” the impeachments. And now that Republicans have wrested back the majority in the chamber, House Speaker Kevin McCarthy says he’s willing to consider such an effort. “I understand why individuals want to do it, and we’d look at it,” McCarthy, California Republican, said in response to a question earlier this month during his first weekly news conference. But his comments raised questions of their own. Namely: Is expunging Trump’s impeachments even possible?The answer, constitutional experts say, is complicated.For one thing, it would be unprecedented in this context.According to the House historian, impeachment proceedings have been initiated more than 60 times in U.S. history against office holders across the federal government, and less than a third of the proceedings have led to full impeachments. That figure includes 15 federal judges, an 18th century senator and a 19th century Cabinet official along with, most notably, President Andrew Johnson in 1868, President Bill Clinton in 1998 and Trump. Eight impeachments – all of them of federal judges – have led to convictions and removals from office by the Senate. None has ever been expunged.'
-})
+print(check({
+    'title':TITLE,
+    'text':TEXT
+}))
--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#
+# Importing required modules
+#
 import nltk
 import pandas as pd
 from nltk.corpus import stopwords
@@ -7,14 +10,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import pickle
 
 
 port_stem = PorterStemmer()
-model = LogisticRegression()
-titlevectorizer = TfidfVectorizer(max_features=50)
-vectorizer = TfidfVectorizer(max_features=50)
 
-
+#
+# Stemming and reducing texts to 50 words at most
+#
 def stem(content):
     stemmed = re.sub('[^a-zA-Z ]', '', content)
     stemmed = stemmed.lower()
@@ -26,10 +29,15 @@ def stem(content):
     return stemmed
 
 if __name__=="__main__":
+
+    # Getting stopwords
     print("Downloading stopwords...")
     nltk.download('stopwords')
     print("Stopwords downloaded")
+
     print("Starting pre-processing...")
+
+    # Reading data and pre-processing
     train_data = pd.read_csv('train.csv')
     train_data.fillna('', inplace=True)
 
@@ -38,23 +46,33 @@ if __name__=="__main__":
     train_data['text'] = train_data['text'].apply(stem)
     print("Text stemmed")
 
-    # train_data.to_csv('modified_train.csv')
-    # train = pd.read_csv('modified_train.csv')
-    # train.fillna('', inplace=True)
-
+    # Vectorizing title and text
+    titlevectorizer = TfidfVectorizer(max_features=50)
+    vectorizer = TfidfVectorizer(max_features=50)
     titlevectorizer.fit(train_data['title'].values)
     title = titlevectorizer.transform(train_data['title'].values)
     title = pd.DataFrame.sparse.from_spmatrix(title)
-    # print(title)
+    print("Title vectorized")
 
-    # print("hsdh")
     vectorizer.fit(train_data['text'].values)
     text = vectorizer.transform(train_data['text'].values)
     text = pd.DataFrame.sparse.from_spmatrix(text)
+    print("Text vectorized")
+
+    # merging both the dataframes into one
     text.columns = [x for x in range(50,100)]
     title = pd.merge(title, text, left_index=True, right_index=True)
-    # print(title)
 
+    # splitting data into test and train
     X_train, X_test, Y_train, Y_test = train_test_split(title, train_data['label'], test_size = 0.2, stratify=train_data['label'], random_state=2)
-
+    
+    model = LogisticRegression()
     model.fit(X_train, Y_train)
+    print("Model finished training")
+    
+    # Storing for later use
+    pickle.dump(titlevectorizer, open("titlevectorizer.pickle", "wb"))
+    pickle.dump(vectorizer, open("vectorizer.pickle", "wb"))
+    pickle.dump(model, open("model.pickle", "wb"))
+
+    print("Model built sucessfully")
